7ecd3982510c8f0e012359a0c7faf60f41855622
hopeless102 <qukla17@gmail.com>
2024-09-09 14:32:58 +0900

    Update zadazhi_C++.cpp

--- zadazhi_C++/zadazhi_C++.cpp
+++ zadazhi_C++/zadazhi_C++.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-
+#include <cmath>
 using  namespace std;
-
 
 int main()
 {
+	setlocale(LC_ALL, "Russian");
+	
 	int a = 30;
 	int b = 5;
 	int c = 0;
@@ -30,9 +31,55 @@ int main()
 
 	cout << "\n a / b =";
 	c = a / b;
-	cout << c;
+	cout << c; 
 
+
+
+	//zadanie 7
+	
+	float a = 0;
+	float b = 0;
+	float c = 0;
+	float d = 0;
+	float e = 0;
+	float r = 0;
+	cout << "Введите количество литров воды номер 1  ";
+		cin >> a ;
+
+	cout << "Введите температуру воды номер 1 ";
+		cin >> b;
+
+
+	cout << "Введите количество литров воды номер 2 ";
+		cin >> c;
+
+	cout << "Введите температуру воды номер 2 ";
+	    cin >> d;
+    
+
+	cout << "Объем смеси: ";
+		e = a + c;
+		 cout << e;
+
+	cout << "\n Температура смеси: ";
+		r = b + d;
+		 cout<< r;
+
+	//zadanie 23в НЕДОДЕЛАНО
+	
+	float q = 0;
+	float w = 0;
+	float e = 0;
+
+	cout << "Введите сторону 1 - ";
+		cin >> q;
+
+	cout << "Введите сторону 2- ";
+		cin >> w;
+	
+	cout << "Введите сторону 3 - ";
+		cin >> e;
+	
 
 	return 0;
-
 }
