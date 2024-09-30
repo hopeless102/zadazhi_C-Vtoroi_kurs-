9c0a03a1e7f1ac8ed01f7097e951b3177840bb73
hopeless102 <qukla17@gmail.com>
2024-09-30 14:29:46 +0900

    Update zadazhi_C++.cpp

--- zadazhi_C++/zadazhi_C++.cpp
+++ zadazhi_C++/zadazhi_C++.cpp
@@ -6,80 +6,90 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 	
-	int a = 30;
-	int b = 5;
-	int c = 0;
-	cout << " a + b =";
-	c = a + b;
-	cout << c;
+	//int a = 30;
+	//int b = 5;
+	//int c = 0;
+	//cout << " a + b =";
+	//c = a + b;
+	//cout << c;
 
 
-	c = 0;
+	//c = 0;
 
-	cout << "\n a - b =";
-	c = a - b;
-	cout << c;
+	//cout << "\n a - b =";
+	//c = a - b;
+	//cout << c;
 
-	c = 0;
+	//c = 0;
 
-	cout << "\n a * b =";
-	c = a * b;
-	cout << c;
+	//cout << "\n a * b =";
+	//c = a * b;
+	//cout << c;
 
 
-	c = 0;
+	//c = 0;
 
-	cout << "\n a / b =";
-	c = a / b;
-	cout << c; 
+	//cout << "\n a / b =";
+	//c = a / b;
+	//cout << c; 
 
 
 
 	//zadanie 7
 	
-	float a = 0;
-	float b = 0;
-	float c = 0;
-	float d = 0;
-	float e = 0;
-	float r = 0;
-	cout << "Введите количество литров воды номер 1  ";
-		cin >> a ;
+	/// float a = 0;
+//	float b = 0;
+	//float c = 0;
+//	float d = 0;
+	//float e = 0;
+	//float r = 0;
+	//cout << "Введите количество литров воды номер 1  ";
+	//cin >> a;
 
-	cout << "Введите температуру воды номер 1 ";
-		cin >> b;
+	//	cout << "Введите температуру воды номер 1 ";
+//	cin >> b;
 
 
-	cout << "Введите количество литров воды номер 2 ";
-		cin >> c;
+	//cout << "Введите количество литров воды номер 2 ";
+	//cin >> c;
 
-	cout << "Введите температуру воды номер 2 ";
-	    cin >> d;
-    
+	//cout << "Введите температуру воды номер 2 ";
+	//cin >> d;
 
-	cout << "Объем смеси: ";
-		e = a + c;
-		 cout << e;
 
-	cout << "\n Температура смеси: ";
-		r = b + d;
-		 cout<< r;
-
-	//zadanie 23в НЕДОДЕЛАНО
+	//cout << "Объем смеси: ";
+	//e = a + c;
+	//	cout << e;
+	//cout << "\n Температура смеси: ";
+	//r = b + d;
+	//cout << r;
 	
-	float q = 0;
-	float w = 0;
-	float e = 0;
-
-	cout << "Введите сторону 1 - ";
-		cin >> q;
-
-	cout << "Введите сторону 2- ";
-		cin >> w;
+	//zadanie 23в
 	
-	cout << "Введите сторону 3 - ";
-		cin >> e;
-	
+	//float a = 0;
+	//float b = 0;
+	//float c = 0;
+//
+	//cout << "Введите сторону 1 - ";
+		//cin >> a;
 
-	return 0;
+		//cout << "Введите сторону 2- ";
+		//cin >> b;
+	
+	//cout << "Введите сторону 3 - ";
+		//cin >> c;
+
+		//using namespace std;
+
+		//double getL(double c, double a, double b); {    // Возвращает длину биссектрисы к стороне A при сторонах A, B, C
+		//	double s = a + b;
+		//	return sqrt(a * b * (s + c) * (s - c)) / s;  // Формула длины биссектрисы через три стороны
+		//}
+
+			//double a, b, c;
+			//cin >> a >> b >> c;
+			//cout << "Длины биссектрис к сторонам A, B и C равны " << getL(a, b, c) << ", " << getL(b, a, c) << " и " << getL(c, a, b) << ".";
+//return 0;
+		
+
 }
